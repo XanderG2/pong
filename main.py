56d5e350a4edae25a3aea7dcc7d6d7ebfa0a4487
XanderG2 <XanderG2@users.noreply.github.com>
2024-01-08 15:55:13 +0000

    direction and hitting and ball

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import pygame, sys
+import pygame, sys, random, math
 pygame.init()
+
+
+def calculate_movement(degrees):
+    radians = math.radians(degrees)
+    x = math.cos(radians)
+    y = math.sin(radians)
+    return x, y
+
 
 width = 900
 height = 500
@@ -15,7 +23,18 @@ pm = height//2-ph//2
 pg = 25
 
 p1 = pygame.Rect(0+pg,pm,pw,ph)
+p1p = 0
 p2 = pygame.Rect(width-pw-pg,pm,pw,ph)
+p2p = 0
+
+ballx = width//2
+ballxd10 = ballx*10
+bally = height//2
+ballyd10 = bally*10
+ballr = 10
+balld = 0
+ballw = 10
+ballhb = pygame.Rect(ballx-ballr, bally-ballr, ballr*2, ballr*2)
 
 p1yd10 = p1.y*10
 p2yd10 = p2.y*10
@@ -38,9 +57,32 @@ while running:
             p1yd10 += 1
     p1.y = p1yd10//10
     p2.y = p2yd10//10
+    if ballhb.colliderect(p1):
+        balld = random.choice([random.randint(0,90), random.randint(270, 359)])
+    if ballhb.colliderect(p2):
+        balld = random.randint(91, 269)
+    if bally == 0 or bally == height-ballr:
+        balld = 360 - balld
+    mx, my = calculate_movement(balld)
+    ballxd10 += mx
+    ballyd10 += my
+    if ballx == 0:
+        p2p += 1
+        ballxd10 = width//2 * 10
+        ballyd10 = height//2 * 10
+    if ballx == width-ballr:
+        p1p += 1
+        ballxd10 = width//2 * 10
+        ballyd10 = height//2 * 10
+    ballx = ballxd10//10
+    bally = ballyd10//10
+    ballhb = pygame.Rect(ballx-ballr, bally-ballr, ballr*2, ballr*2)
     screen.fill(black)
     pygame.draw.rect(screen, white, p1)
     pygame.draw.rect(screen, white, p2)
+    pygame.draw.rect(screen, (255,0,0), ballhb)
+    pygame.draw.circle(screen, (255,255,255), (ballx, bally), ballr, ballw)
     pygame.display.flip()
+    print(f"Player 1 points: {p1p}, Player 2 points: {p2p}")
 
 pygame.quit()
